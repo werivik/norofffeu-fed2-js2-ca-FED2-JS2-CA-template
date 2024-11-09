696993a8d5da2e1e0fed76b17c3dbe74d2d2a01b
werivik <wervik00994@stud.noroff.no>
2024-11-09 13:25:08 +0100

    responsivity
    Checked through the pages and made it more responsive to smaller screens, and saw I didnt code the main index page so I also did that :)

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-bg': '#ECF0F6',
+      },
+      screens: {
+
+      },
+    },
+  },
+  plugins: [],
+}
+
